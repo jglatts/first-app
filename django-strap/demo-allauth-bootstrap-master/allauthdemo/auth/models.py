a02f73b747b1b1baa77506a15f8ceeb38c864480
John Glatts <33476958+jglatts@users.noreply.github.com>
2018-08-06 02:00:22 -0400

    Update models.py

--- django-strap/demo-allauth-bootstrap-master/allauthdemo/auth/models.py
+++ django-strap/demo-allauth-bootstrap-master/allauthdemo/auth/models.py
@@ -126,6 +126,7 @@ class User(AbstractBaseUser, PermissionsMixin):
             pass
         else:
             url = "https://www.drudgereport.com"
+            # add headers
             headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'}
             src = requests.get(url)
             pln_txt = src.text
