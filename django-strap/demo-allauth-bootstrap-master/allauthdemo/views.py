1533e0807d274e6876ecca07ea6f013f4db71a63
John Glatts <33476958+jglatts@users.noreply.github.com>
2018-08-09 22:34:21 -0400

    Update views.py

--- django-strap/demo-allauth-bootstrap-master/allauthdemo/views.py
+++ django-strap/demo-allauth-bootstrap-master/allauthdemo/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader, RequestContext
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
-
+from .auth.models import Question 
 
 @login_required
 def member_index(request):
@@ -16,11 +16,14 @@ def member_action(request):
     c = {}  #{'foo': 'bar'}
     return HttpResponse(t.render(c, request), content_type='text/html')
 
-
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('visitor/question1.html')
+    template = loader.get_template('visitor/question.html')
     context = {
         'latest_question_list': latest_question_list,
     }
     return HttpResponse(template.render(context, request))
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'visitor/detail.html', {'question': question})
